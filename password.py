467296f1f657410d8ecea8baffca1004659df013
Norah Kim <norahk1049@gmail.com>
2025-02-11 19:03:41 +0000

    password

--- password.py
+++ password.py
@@ -24,8 +24,14 @@ def fetch_word():
     url = "https://random-word.herokuapp.com/word?length=6"
 
     response = requests.get(url)
-    word = response.jfson()[0]
+    word = response.json()[0]
     return word
 
-print(fetch_word())
+#function to generate weaker but memorable password
+def generate_weaker_password():
+    word1 = fetch_word()
+    word2 = fetchword()
+    password = word1+ word2
+    return password
 
+print(generate_weaker_password())
